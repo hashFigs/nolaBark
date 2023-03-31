d1d1769205a6b45855332d31304698985532bbaa
jordimintknight <93216318+jordimintknight@users.noreply.github.com>
2023-03-31 00:11:03 -0500

    newsletter layout

--- pages/services.jsx
+++ pages/services.jsx
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 import DogWalking from "../components/services/dog-walking";
 import MainBanner from "../components/mainBanner";
 import Benefits from "../components/services/benefits";
+import ServicesBanner from "../components/home/servicesBanner";
 
 export default function Services({ allPostsData }) {
   return (
@@ -14,17 +15,11 @@ export default function Services({ allPostsData }) {
             height = {"60"}
             anchor = {"bottom"}
         />      
-        <DogWalking />
-        <Benefits 
-            title ={"ALL WALKS INCLUDE"}
-            image = {"pack.jpg"}
-            description = {["Easy App-based Booking", 
-                            "PUPort showing photos, GPS map, details of the walk",
-                            "Back-up safety collar",
-                            "Use of eco-friendly poop bags",
-                            "Water refreshment",
-        ]}
-        />
+
+
+        <ServicesBanner />
+
+        
         
       </Layout>
     </>
