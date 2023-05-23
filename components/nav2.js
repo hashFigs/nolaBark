fe709384c2b62877fd5ea4ca6d97f60469d08edf
jordimintknight <93216318+jordimintknight@users.noreply.github.com>
2023-05-22 22:33:01 -0500

    removing login

--- components/nav2.js
+++ components/nav2.js
@@ -88,9 +88,7 @@ return(
     
   </Navbar.Collapse>
   
-  <Link href="/login" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
-    Login
-  </Link>
+
   
 </Navbar>
 
